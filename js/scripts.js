af41f31059035863e54de9c7d3f7d460afd5ff98
justinbeaudry10 <justinbeaudry10@gmail.com>
2021-10-09 17:12:56 -0400

    Added odds conversion, status to recent bets table

--- js/scripts.js
+++ js/scripts.js
@@ -4,69 +4,135 @@
  * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
  */
 
-// Scripts
-
-// Data
-
-const betsData = [];
-
-// Elements
-
-const btnAddBet = document.querySelector(".btn-add-bet");
-const btnConfirm = document.querySelector(".btn-confirm-bet");
-const btnCancel = document.querySelector(".btn-cancel");
-const addBetContainer = document.querySelector(".add-bet-container");
-const addBetForm = document.querySelector(".add-bet");
-const sportsbook = document.querySelector("#sportsbook");
-const league = document.querySelector("#league");
-const sportEvent = document.querySelector("#event");
-const selection = document.querySelector("#selection");
-const odds = document.querySelector("#odds");
-const risk = document.querySelector("#risk");
-const recentBetsTable = document.querySelector("#datatablesSimple");
-
-// Functions
-
-function addNewBet(sb, lg, ev, sl, odd, risk) {
-  let markup = `
-  <tr>
-    <td>${sb}</td>
-    <td>${lg}</td>
-    <td>${ev}</td>
-    <td>${sl}</td>
-    <td>${odd}</td>
-    <td>${risk}</td>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-  `;
-  recentBetsTable.insertAdjacentHTML("afterbegin", markup);
-}
-
-// Event Listeners
-
-btnAddBet.addEventListener("click", (e) => {
-  addBetContainer.classList.remove("d-none");
-});
-
-btnCancel.addEventListener("click", (e) => {
-  addBetContainer.classList.add("d-none");
-});
-
-btnConfirm.addEventListener("click", (e) => {
-  e.preventDefault();
-  let curBook = sportsbook.value;
-  let curLeague = league.options[league.selectedIndex].value;
-  let curEvent = sportEvent.value;
-  let curSelection = selection.value;
-  let curOdds = odds.value;
-  let curRisk = risk.value;
-  addNewBet(curBook, curLeague, curEvent, curSelection, curOdds, curRisk);
-  addBetContainer.classList.add("d-none");
-});
-
 window.addEventListener("DOMContentLoaded", (event) => {
+  // Classes
+  class Bet {
+    constructor(sb, lg, ev, sl, style, odd, risk, status = "Unsettled") {
+      this.book = sb;
+      this.league = lg;
+      this.event = ev;
+      this.selection = sl;
+      this.risk = risk;
+      this.status = status;
+
+      // Odds are stored in decimal format
+      switch (style) {
+        case "American":
+          let mult = +odd.slice(1, odd.length);
+          if (odd[0] === "+") {
+            this.odds = (mult / 100 + 1).toFixed(2);
+          } else if (odd[0] === "-") {
+            this.odds = (100 / mult + 1).toFixed(2);
+          }
+          break;
+        case "Decimal":
+          this.odds = (+odd).toFixed(2);
+          break;
+        case "Fractional":
+          let nums = odd.split("/");
+          this.odds = (+nums[0] / +nums[1] + 1).toFixed(2);
+          break;
+      }
+    }
+
+    setStatus(newStatus) {
+      this.status = newStatus;
+    }
+  }
+
+  // Data
+
+  const betsData = [];
+
+  // Elements
+
+  const btnAddBet = document.querySelector(".btn-add-bet");
+  const btnConfirm = document.querySelector(".btn-confirm-bet");
+  const btnCancel = document.querySelector(".btn-cancel");
+  const addBetContainer = document.querySelector(".add-bet-container");
+  const addBetForm = document.querySelector(".add-bet");
+  const sportsbook = document.querySelector("#sportsbook");
+  const league = document.querySelector("#league");
+  const sportEvent = document.querySelector("#event");
+  const selection = document.querySelector("#selection");
+  const oddsStyle = document.querySelector("#style");
+  const odds = document.querySelector("#odds");
+  const risk = document.querySelector("#risk");
+  const recentBetsTable = document.querySelector("#datatablesSimple");
+  let statusSelect = document.getElementsByClassName("status-select");
+
+  // Functions
+
+  function addNewBet(bet) {
+    let markup = `
+    <tr class="${bet.status}">
+      <td>${bet.book}</td>
+      <td>${bet.league}</td>
+      <td>${bet.event}</td>
+      <td>${bet.selection}</td>
+      <td>${bet.odds}</td>
+      <td>${bet.risk}</td>
+      <td></td>
+      <td></td>
+      <td>
+        <select class="status-select">
+          <option value="unsettled">Unsettled</option>
+          <option value="won">Won</option>
+          <option value="lost">Lost</option>
+          <option value="void">Void</option>
+          <option value="cashedOut">Cashed Out</option>
+        </select>
+      </td>
+    </tr>
+    `;
+    recentBetsTable.insertAdjacentHTML("afterbegin", markup);
+    updateStatusArr();
+  }
+
+  // Event Listeners
+
+  // Keeps an updated array of rows in the table and adds event listeners to all of them to check their status
+  function updateStatusArr() {
+    let statusArr = Array.from(statusSelect);
+
+    statusArr.forEach((bet) => {
+      bet.addEventListener("change", (e) => {
+        e.target.parentElement.parentElement.className = e.target.value;
+      });
+    });
+  }
+  updateStatusArr(); // Populates array with initial values already in table
+
+  btnAddBet.addEventListener("click", (e) => {
+    addBetContainer.classList.remove("d-none");
+  });
+
+  btnCancel.addEventListener("click", (e) => {
+    addBetContainer.classList.add("d-none");
+  });
+
+  btnConfirm.addEventListener("click", (e) => {
+    e.preventDefault();
+    let curBook = sportsbook.value;
+    let curLeague = league.options[league.selectedIndex].value;
+    let curEvent = sportEvent.value;
+    let curSelection = selection.value;
+    let curStyle = style.options[style.selectedIndex].value;
+    let curOdds = odds.value;
+    let curRisk = risk.value;
+    const newBet = new Bet(
+      curBook,
+      curLeague,
+      curEvent,
+      curSelection,
+      curStyle,
+      curOdds,
+      curRisk
+    );
+    addNewBet(newBet);
+    addBetContainer.classList.add("d-none");
+  });
+
   // Toggle the side navigation
   const sidebarToggle = document.body.querySelector("#sidebarToggle");
   if (sidebarToggle) {
